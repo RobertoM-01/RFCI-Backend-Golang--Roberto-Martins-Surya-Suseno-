62b0261c8b9fad913a3dd58de2f5ff73d3caaa7a
RobertoM-01 <58824556+RobertoM-01@users.noreply.github.com>
2020-09-09 21:42:37 +0700

    Soal nomor 1

---
+++ Sorting.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("[7.9]", "->", 4795893)
+	fmt.Println("[5.9]", "->", 4759893)
+	fmt.Println("[5.7]", "->", 4759893)
+	fmt.Println("[8.9]", "->", 4759893)
+	fmt.Println("[3.9]", "->", 4759893)
+	fmt.Println("[3.9]", "->", 4759399)
+	fmt.Println("[3.8]", "->", 4573899)
+	fmt.Println("[3.7]", "->", 4537899)
+	fmt.Println("[3.5]", "->", 4357899)
+	fmt.Println("[3.4]", "->", 3457899)
+}
